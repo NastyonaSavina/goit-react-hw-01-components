0f93dcbae6d369ed83f1d1a5d427ffe72ea771e5
NastyonaSavina <nastyona.savina@gmail.com>
2022-12-21 23:19:57 +0200

    update_21.12

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -6,8 +6,8 @@ export function Container({ children }) {
   return <div className={styles.container}>{children}</div>;
 }
 
-// Container.propTypes = {
-//     children: PropTypes.array,
-// };
+Container.propTypes = {
+    children: PropTypes.array,
+};
 
 export default Container;
